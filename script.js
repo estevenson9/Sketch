b389fadfa2a7e2bf787bb59d2e6574a4c639ea8c
estevenson <149737134+estevenson9@users.noreply.github.com>
2024-02-02 11:25:38 +1000

    Add default class and specific ID's for each grid square

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const mainContent = document.querySelector(".main-content");
 const NUM_OF_GRIDS = 16;
 let gridContainer = document.createElement("div");
+gridContainer.className = "grid-container";
 
 for (let index = 0; index < NUM_OF_GRIDS; index++) {
   let gridSquare = document.createElement("div");
   gridSquare.textContent = index.toString();
+  gridSquare.className = `grid-square`;
+  gridSquare.id = `grid-${index}`;
   gridContainer.appendChild(gridSquare);
 }
 mainContent.appendChild(gridContainer);
