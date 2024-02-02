4d8988ae6a2065e98815860fff6b192a607e4d9a
estevenson <149737134+estevenson9@users.noreply.github.com>
2024-02-02 12:29:50 +1000

    Fixed grid, implemented styling

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const mainContent = document.querySelector(".main-content");
-const NUM_OF_GRIDS = 16;
+const SIZE_OF_GRID = 16;
 let gridContainer = document.createElement("div");
 gridContainer.className = "grid-container";
 
-for (let index = 0; index < NUM_OF_GRIDS; index++) {
-  let gridSquare = document.createElement("div");
-  gridSquare.textContent = index.toString();
-  gridSquare.className = `grid-square`;
-  gridSquare.id = `grid-${index}`;
-  gridContainer.appendChild(gridSquare);
+for (let rowNum = 0; rowNum < SIZE_OF_GRID; rowNum++) {
+  let gridRow = document.createElement("div");
+  gridRow.className = `grid-row`;
+  for (let colNum = 0; colNum < SIZE_OF_GRID; colNum++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.textContent = `.`;
+    gridSquare.className = `grid-square`;
+    gridSquare.id = `grid-${colNum + 16 * rowNum}`;
+    gridRow.appendChild(gridSquare);
+  }
+  gridContainer.appendChild(gridRow);
 }
+
+// let testRow = document.createElement("div");
+// for (let index = 1; index < SIZE_OF_GRID ** 2 + 1; index++) {
+//   let testSquare = document.createElement("div");
+//   testSquare.textContent = `${index}`;
+//   testSquare.className = `grid-square`;
+//   testSquare.id = `grid-${index}`;
+//   testRow.appendChild(testSquare);
+//   if (index % SIZE_OF_GRID === 0) {
+//     gridContainer.appendChild(testRow);
+//     testRow = document.createElement("div");
+//   }
+// }
 mainContent.appendChild(gridContainer);
