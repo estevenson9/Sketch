a01a2b079abe776a9e5d131d1ebf6e478edca41b
estevenson <149737134+estevenson9@users.noreply.github.com>
2024-02-02 15:05:41 +1000

    Add colouring in of grid via mousedown and drag

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const mainContent = document.querySelector(".main-content");
-const SIZE_OF_GRID = 16;
+const gridSizeInput = document.querySelector("#grid-size");
+let SIZE_OF_GRID = 16;
 let gridContainer = document.createElement("div");
-gridContainer.className = "grid-container";
-function hoverEffect() {
-  this.style.cssText = "background-color: blue";
+gridSizeInput.addEventListener("input", () => {
+  SIZE_OF_GRID = gridSizeInput.value;
+  if (SIZE_OF_GRID > 128 || SIZE_OF_GRID < 16) {
+    SIZE_OF_GRID = 16;
+  }
+  if (mainContent.contains(gridContainer)) {
+    mainContent.removeChild(gridContainer);
+  }
+  gridContainer = document.createElement("div");
+  gridContainer.className = "grid-container";
+  drawGrid(SIZE_OF_GRID, gridContainer);
+});
+
+function colorIn() {
+  this.style.backgroundColor = "blue";
 }
 
-for (let rowNum = 0; rowNum < SIZE_OF_GRID; rowNum++) {
-  let gridRow = document.createElement("div");
-  gridRow.className = `grid-row`;
-  for (let colNum = 0; colNum < SIZE_OF_GRID; colNum++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.addEventListener("mouseenter", hoverEffect);
-    gridSquare.textContent = `.`;
-    gridSquare.className = `grid-square`;
-    gridSquare.id = `grid-${colNum + 16 * rowNum}`;
-    gridRow.appendChild(gridSquare);
+function eraseOut() {
+  this.style.backgroundColor = "";
+}
+function drawGrid(gridsize, container) {
+  for (let rowNum = 0; rowNum < gridsize; rowNum++) {
+    let gridRow = document.createElement("div");
+    gridRow.className = `grid-row`;
+    for (let colNum = 0; colNum < gridsize; colNum++) {
+      let gridSquare = document.createElement("div");
+      container.addEventListener("mousedown", () => {
+        gridSquare.addEventListener("mousemove", colorIn);
+      });
+      container.addEventListener("mouseup", () => {
+        gridSquare.removeEventListener("mousemove", colorIn);
+      });
+      gridSquare.textContent = `.`;
+      gridSquare.className = `grid-square`;
+      gridSquare.id = `grid-${colNum + 16 * rowNum}`;
+      gridSquare.style.cssText = `width: ${800 / gridsize}px;
+      height: ${800 / gridsize}px; user-select: none`;
+      gridRow.appendChild(gridSquare);
+    }
+    container.appendChild(gridRow);
   }
-  gridContainer.appendChild(gridRow);
+  mainContent.appendChild(container);
 }
 
 // let testRow = document.createElement("div");
@@ -32,4 +58,3 @@ for (let rowNum = 0; rowNum < SIZE_OF_GRID; rowNum++) {
 //     testRow = document.createElement("div");
 //   }
 // }
-mainContent.appendChild(gridContainer);
